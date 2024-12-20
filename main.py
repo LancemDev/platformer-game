a9ad1d61bd3450a6298d6d4ce5f1fca0fc2ed630
Edith Haggai <131600246+Edith-Haggai@users.noreply.github.com>
2024-12-20 14:29:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ pygame.display.set_caption(GAME_NAME)
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+from modules.player import Player
+from modules.fire import Fire
+from modules.terrain import create_random_terrain, create_random_target_block
+from modules.utils import get_background
+from modules.collision import handle_move, check_congratulation_collision
+
+def draw(window, background, bg_image, player, objects, offset_x):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    for obj in objects:
+        obj.draw(window, offset_x)
+
+    player.draw(window, offset_x)
+
+    pygame.display.update()
 
 
 
