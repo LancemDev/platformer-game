d74fb672fef0ded00be52d6b81f2323e8c66eb76
Shevaun <143563926+o-shevaun@users.noreply.github.com>
2024-12-20 14:34:10 +0300

    handling events main.py

--- main.py
+++ main.py
@@ -26,6 +26,30 @@ def draw(window, background, bg_image, player, objects, offset_x):
 
 
 
+def handle_events(player):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player.jump_count < 2:
+                player.jump()
+    return True
+
+
+# Handle camera scrolling
+def handle_scrolling(player):
+    # if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or \
+    #    ((player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
+    #     offset_x += player.x_vel
+        
+    """
+    Calculate offset to center the camera on the player.
+    """
+    offset_x = player.rect.centerx - WIDTH // 2
+    return offset_x
+
+
+
 if __name__ == "__main__":
     window = pygame.display.set_mode((WIDTH, HEIGHT))
     main(window)
